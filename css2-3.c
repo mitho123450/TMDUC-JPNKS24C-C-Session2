ad17896d327e68d40d6b4b71e0421a9eca8e26fa
mitho123450 <mitho123450@gmail.com>
2024-11-21 03:03:35 +0700

    upload bà tập session1-3

---
+++ css2-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+  // khai bao va khoi tao hai bien so nguyen
+  int a = 5;
+  int b = 10;
+  // tinh hieu cua a va b
+  int minus = a - b; 
+  // in ket qua ra man hinh
+  printf("hieu cua a va b la: minus ");
+  // tinh tich cua a va b 
+  int multiplication = a * b;
+  printf("tich cua a va b la: multiplication ");
+  // tinh thuong cua a va b
+  int division = a / b;
+  // in ket qua ra man hinh 
+  printf("thuong cua a va b la: division");
+return 0;} 
