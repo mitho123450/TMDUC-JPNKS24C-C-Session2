3bcf86c965c8b0091b2bff7ea802535adcc500ae
mitho123450 <mitho123450@gmail.com>
2024-11-21 02:47:18 +0700

    upload bai tập session2-1

---
+++ css2-1.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+
+int main(){
+	// kieu int: khai bao va luu tru gia tri la so nguyen 
+	int a = 5; // khai bao bien a voi gia tri la 5
+	// kiêu fload: dung de khai bao bien so thuc
+	float b = 3.14; // khai bao bien b voi gia tri la 3.14
+	// kieu double: dung de khai bao bien so thuc, do chinh xac cao hon float
+	double c = 3.1437498; // khai bao bien c voi gia tri la 3.1437498
+	// kieu char: dung de khai bao bien ki tu, co the luu tru mot ki tu duy nhat
+	char d = 'A'; // khai bao bien d voi gia tri la ki tu "A" 
+	// in ra gia tri cac bien
+	printf("a (int) = khai bao va luu tru gia tri la so nguyen ");
+	printf("b (float) = dung de khai bao bien so thuc ");
+	printf("c (double) = dung de khai bao bien so thuc, do chinh xac cao hon float ");
+	printf("d (char) = dung de khai bao bien ki tu, co the luu tru mot ki tu duy nhat ");
+	
+	return 0;
+	 
+}
