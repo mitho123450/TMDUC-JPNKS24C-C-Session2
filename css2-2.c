99c470b4dceb321a37caf7e1f224842787b02bd7
mitho123450 <mitho123450@gmail.com>
2024-11-21 02:17:06 +0700

    upload bài tập session2-2

---
+++ css2-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+  // khai bao va khoi tao hai bien so nguyen
+  int a = 5;
+  int b = 10;
+  // tinh tong cua a va b
+  int c = a + b; 
+  // in ket qua ra man hinh
+  printf("tong cua a va b la: c");
+   
+  return 0; 
+}
